3db64c5b08c97873983395c90ddd06a7c65509d9
Alessio Galatolo <galatoloalessio@gmail.com>
2019-10-07 18:13:43 +0200

    Added monitor to Patient visit

--- src/Patient.java
+++ src/Patient.java
@@ -1,11 +1,30 @@
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 public class Patient extends Thread {
 
-    public Patient(){
 
+    private final ArrayList<Doctor> doctors;
+
+    public Patient(ArrayList<Doctor> d) {
+        this.doctors = d;
     }
 
     @Override
     public void run() {
+        for(Doctor d: doctors){
+            d.startVisit();
+        }
 
+        try {
+            Thread.sleep(GregorianCalendar.getInstance().getTimeInMillis() % 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //TODO: patient returns again
+
+        for(Doctor d: doctors){
+            d.endVisit();
+        }
     }
 }
